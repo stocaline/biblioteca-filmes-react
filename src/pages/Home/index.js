285eeedf0e5ecbc84ab10652f5f624a6cedff7b6
RichardSB <richardbercheli@gmail.com>
2023-10-05 20:16:38 -0300

    Adicionando botão para mais filmes

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -8,29 +8,27 @@ const apiKey = process.env.REACT_APP_API_KEY;
 function Home() {
     const [filmes, setFilmes] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [page, setPage] = useState(1)
+
+    async function loadFilmes() {
+        const response = await api.get("movie/now_playing", {
+            params: {
+                api_key: `${apiKey}`,
+                language: "pt-BR",
+                page: page,
+            }
+        })
+        setPage(page + 1)
+        setFilmes([...filmes, ...response.data.results.slice(0, 10)])
+        setLoading(false);
+    }
 
     useEffect(() => {
-
-        async function loadFilmes() {
-            const response = await api.get("movie/now_playing", {
-                params: {
-                    api_key: `${apiKey}`,
-                    language: "pt-BR",
-                    page: 1,
-                }
-            })
-
-            //console.log(;
-            setFilmes(response.data.results.slice(0, 10))
-            setLoading(false);
-        }
-
-        loadFilmes();
-
+        loadFilmes()
     }, [])
 
-    if(loading){
-        return(
+    if (loading) {
+        return (
             <div className="loading">
                 <h2>Carregando filmes....</h2>
             </div>
@@ -44,12 +42,13 @@ function Home() {
                     return (
                         <article key={filme.id}>
                             <strong>{filme.title}</strong>
-                            <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title}/>
+                            <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title} />
                             <Link to={`/filme/${filme.id}`}>Acessar</Link>
                         </article>
                     )
                 })}
             </div>
+            <button className='btn-more-itens' onClick={() => loadFilmes()}>Carregar mais</button>
         </div>
     )
 }
